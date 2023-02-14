67b108b1f99db72fc1639a61261d6775155a8c2f
Liplum <Li_plum@outlook.com>
2023-02-14 12:35:02 +0800

    load configuration

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,15 +1,51 @@
 import { createServer } from 'http'
 import { Video } from 'homecasting-shared/model/video'
-const hostname = '127.0.0.1'
-const port = 53552
+import * as fs from 'fs'
 
-const server = createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World\n')
-});
+var config = {
+  hostname: '127.0.0.1',
+  port: 53552,
+}
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+const configFileName = "homecasting-config.json"
+function findConfig() {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  let curDir = path.dirname(__dirname)
+  let configFile: string | null = findFileInFileTree(curDir, configFileName)
+  if (configFile == null) {
+    configFile = path.join(curDir, configFileName)
+    fs.writeFileSync(configFile, JSON.stringify(config, null, 2));
+  }
+  let rawdata = fs.readFileSync(configFile).toString();
+  config = JSON.parse(rawdata);
+}
+
+function findFileInFileTree(dir: string, fileName: string): string | null {
+  let lastDir: string | null = null
+  while (dir != lastDir) {
+    const configFile = path.join(dir, fileName)
+    if (fs.existsSync(configFile)) {
+      return configFile
+    } else {
+      lastDir = dir
+      dir = path.dirname(dir)
+    }
+  }
+  return null
+}
+
+function startServer() {
+  const server = createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello World\n')
+  });
 
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
-});
+  server.listen(config.port, config.hostname, () => {
+    console.log(`Server running at http://${config.hostname}:${config.port}/`)
+  });
+}
+findConfig()
+startServer()
