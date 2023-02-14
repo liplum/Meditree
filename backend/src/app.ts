54418e80186f9ad0ac9573753cab67d250da2452
Liplum <Li_plum@outlook.com>
2023-02-14 08:38:02 +0800

    backend with shared project

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,15 @@
-import { createServer } from 'http';
-//import {Video} from 'homecasting-shared';
-const hostname = '127.0.0.1';
-const port = 3000;
+import { createServer } from 'http'
+import { Video } from 'homecasting-shared/model/video'
+const hostname = '127.0.0.1'
+const port = 53552
 
 const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World\n')
 });
 
+
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`Server running at http://${hostname}:${port}/`)
 });
--- shared/src/model/Video.ts
+++ shared/src/model/Video.ts
@@ -1,3 +1,3 @@
-interface Video {
+export interface Video {
   url: String
 }
