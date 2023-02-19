4332951f18270af057b846bc383dbdc0359721e3
Liplum <Li_plum@outlook.com>
2023-02-19 16:50:39 +0800

    ios safari video streaming support

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -70,33 +70,84 @@ function removePrefix(origin: string, prefix: string): string {
 }
 
 function getVideo(req: Request, res: Response, file: File) {
-  const videoPath = file.path
-  const stat = fs.statSync(videoPath)
-  const videoSize = stat.size
-  const range = req.headers.range;
-  if (!req.headers.range) {
-    res.header({
-      'Content-Length': videoSize,
-      'Content-Type': file.type,
-    })
-    res.status(200)
-    const stream = fs.createReadStream(videoPath)
-    stream.pipe(res)
-  } else {
-    const CHUNK_SIZE = 10 ** 6
-    const start = Number(range.replace(/\D/g, ""))
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1)
-    const contentLength = end - start + 1
-    res.header({
-      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-      "Accept-Ranges": "bytes",
-      "Content-Length": contentLength,
-      "Content-Type": file.type,
-    })
-    res.status(206)
-    const videoStream = fs.createReadStream(videoPath, { start, end })
-    videoStream.pipe(res)
+  const filePath = file.path
+  const options = {
+    start: undefined,
+    end: undefined,
   }
+
+  let start: number
+  let end: number
+
+  const range = req.headers.range
+  if (range) {
+    const bytesPrefix = "bytes="
+    if (range.startsWith(bytesPrefix)) {
+      const bytesRange = range.substring(bytesPrefix.length)
+      const parts = bytesRange.split("-")
+      if (parts.length === 2) {
+        const rangeStart = parts[0] && parts[0].trim()
+        if (rangeStart && rangeStart.length > 0) {
+          options.start = start = parseInt(rangeStart)
+        }
+        const rangeEnd = parts[1] && parts[1].trim()
+        if (rangeEnd && rangeEnd.length > 0) {
+          options.end = end = parseInt(rangeEnd)
+        }
+      }
+    }
+  }
+
+  res.setHeader("content-type", "video/mp4")
+
+  fs.stat(filePath, (err, stat) => {
+    if (err) {
+      console.error(`File stat error for ${filePath}.`)
+      console.error(err)
+      res.sendStatus(500)
+      return
+    }
+
+    let contentLength = stat.size
+
+    if (req.method === "HEAD") {
+      res.statusCode = 200
+      res.setHeader("accept-ranges", "bytes")
+      res.setHeader("content-length", contentLength)
+      res.end()
+    }
+    else {
+      let retrievedLength: number
+      if (start !== undefined && end !== undefined) {
+        retrievedLength = (end + 1) - start
+      }
+      else if (start !== undefined) {
+        retrievedLength = contentLength - start
+      }
+      else if (end !== undefined) {
+        retrievedLength = (end + 1)
+      }
+      else {
+        retrievedLength = contentLength
+      }
+
+      res.statusCode = start !== undefined || end !== undefined ? 206 : 200
+
+      res.setHeader("content-length", retrievedLength)
+
+      if (range !== undefined) {
+        res.setHeader("content-range", `bytes ${start || 0}-${end || (contentLength - 1)}/${contentLength}`)
+        res.setHeader("accept-ranges", "bytes")
+      }
+
+      const fileStream = fs.createReadStream(filePath, options)
+      fileStream.on("error", error => {
+        res.sendStatus(500)
+      })
+
+      fileStream.pipe(res)
+    }
+  })
 }
 
 function getImage(req: Request, res: Response, file: File) {
