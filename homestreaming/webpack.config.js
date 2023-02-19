8b451dee45352598fe9ee83823d8c05db9a95636
Liplum <Li_plum@outlook.com>
2023-02-19 23:20:56 +0800

    test webpack

--- homestreaming/webpack.config.js
+++ homestreaming/webpack.config.js
@@ -1,10 +1,21 @@
+var mode = process.env.NODE_ENV || 'development';
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
+  mode: mode,
+  devtool: (mode === 'development') ? 'inline-source-map' : false,
   entry: path.join(__dirname, "src", "index.js"),
   output: {
     path: path.resolve(__dirname, "dist"),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 8080,
   },
   module: {
     rules: [
@@ -31,7 +42,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"),
+      template: path.join(__dirname, "public", "index.html"),
     }),
   ],
 }
