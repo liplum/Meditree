50f4e53cb36988c581931b24434bcbfb80672946
Liplum <Li_plum@outlook.com>
2023-02-19 13:25:24 +0800

    try webpack

--- homestreaming/webpack.config.js
+++ homestreaming/webpack.config.js
@@ -1,5 +1,37 @@
-const path = require('path');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }, {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      }, {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ['file-loader'],
+      }, {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
 }
