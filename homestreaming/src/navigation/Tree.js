583b2b4979d2722155a2db6a4015fc233d280bec
Liplum <Li_plum@outlook.com>
2023-02-20 12:02:28 +0800

    Tree view of file tree

--- homestreaming/src/navigation/Tree.js
+++ homestreaming/src/navigation/Tree.js
@@ -5,26 +5,62 @@ import ChevronRightIcon from '@mui/icons-material/ChevronRight'
 import TreeItem from '@mui/lab/TreeItem'
 
 export class FileTreeNavigation extends React.Component {
+
   constructor(props) {
     super(props)
+    this.renderObject = createTreeViewRenderObject(this.props.fileTree)
   }
   render() {
-    //return <div>{JSON.stringify(this.props.fileTree)}</div>
+    const renderTree = (nodes) => (
+      <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
+        {Array.isArray(nodes.children)
+          ? nodes.children.map((node) => renderTree(node))
+          : null}
+      </TreeItem>
+    );
     return <TreeView
       aria-label="file system navigator"
       defaultCollapseIcon={<ExpandMoreIcon />}
       defaultExpandIcon={<ChevronRightIcon />}
-      sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
+      defaultExpanded={['root']}
+      sx={{ height: 110, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
     >
-      <TreeItem nodeId="1" label="Applications">
-        <TreeItem nodeId="2" label="Calendar" />
-      </TreeItem>
-      <TreeItem nodeId="5" label="Documents">
-        <TreeItem nodeId="10" label="OSS" />
-        <TreeItem nodeId="6" label="MUI">
-          <TreeItem nodeId="8" label="index.js" />
-        </TreeItem>
-      </TreeItem>
+      {renderTree(this.renderObject)}
     </TreeView>
   }
+
+}
+
+function createTreeViewRenderObject(fileTree) {
+  const rootChildren = []
+  const rootObj = {
+    id: "root",
+    name: "MyDirectory",
+    children: rootChildren
+  }
+  let id = 1
+  function createNode(children, fileTree) {
+    for (const [name, file] of Object.entries(fileTree)) {
+      let curId = `${id++}`
+      if (file instanceof Object) {
+        // if file is an object, it presents a directory
+        const myChildren = []
+        const obj = {
+          id: curId,
+          name,
+          children: myChildren
+        }
+        children.push(obj)
+        createNode(myChildren, file)
+      } else {
+        // otherwise, it presents a file
+        children.push({
+          id: curId,
+          name
+        })
+      }
+    }
+  }
+  createNode(rootChildren, fileTree)
+  return rootObj
 }
