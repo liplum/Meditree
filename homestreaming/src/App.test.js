034879c397f9d5d9d27784b13fc9034d94863363
Liplum <Li_plum@outlook.com>
2023-03-16 14:35:45 +0800

    convert `.js` to `.jsx`

--- homestreaming/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import { HomestreamingApp } from './App'
-
-test('renders learn react link', () => {
-  render(<HomestreamingApp />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
-})
