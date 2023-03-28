c2c2dd05a716fc59a1e40594d97b6972b409d6aa
Liplum <Li_plum@outlook.com>
2023-03-28 18:33:51 +0800

    [backend-go] new file tree struct

--- backend-go/main.go
+++ backend-go/main.go
@@ -10,68 +10,77 @@ import (
 
 type FileType = string
 type FileTree struct {
-	Name     string
-	Path     string
-	Type     FileType
-	Size     int64
-	Children []*FileTree
+	Name      string
+	Path      string
+	Type      FileType
+	Size      int64
+	Name2File map[string]*FileTree
+}
+
+func (tree *FileTree) String() string {
+	return fmt.Sprintf("%s [%s]", tree.Name, tree.Type)
 }
 
 func (tree *FileTree) ToJson() map[string]any {
 	res := make(map[string]any)
-	for _, file := range tree.Children {
-		if file.Children == nil {
-			res[file.Name] = map[string]any{
-				"size": file.Size,
+	for name, file := range tree.Name2File {
+		if file.Name2File == nil {
+			res[name] = map[string]any{
 				"type": file.Type,
+				"size": file.Size,
 			}
 		} else {
-			res[file.Name] = file.ToJson()
+			res[name] = file.ToJson()
 		}
 	}
 	return res
 }
-func CreateFileTree(path string, classifier func(path string) FileType) (*FileTree, error) {
-	// Initialize the file tree
-	tree := &FileTree{
-		Name: filepath.Base(path),
-		Path: path,
-	}
 
-	// Read the directory contents
-	files, err := os.ReadDir(path)
-	if err != nil {
-		return nil, err
-	}
+func CreateFileTree(root string, classifier func(path string) FileType) *FileTree {
+	var createSubTree func(tree *FileTree, curDir string)
+	createSubTree = func(tree *FileTree, curDir string) {
+		// Read the directory contents
+		files, err := os.ReadDir(curDir)
+		if err != nil {
+			return
+		}
 
-	// Recursively generate the file tree for each file/directory in the directory
-	for _, f := range files {
-		childPath := filepath.Join(path, f.Name())
+		// Recursively generate the file tree for each file/directory in the directory
+		for _, f := range files {
+			fileName := f.Name()
+			childPath := filepath.Join(curDir, fileName)
 
-		// If the child is a directory, generate its file tree recursively
-		if f.IsDir() {
-			child, err := CreateFileTree(childPath, classifier)
-			if err != nil {
-				return nil, err
-			}
-			tree.Children = append(tree.Children, child)
-		} else {
-			// If the child is a file, add it to the file tree
-			if fileType := classifier(childPath); fileType != "" {
-				if info, err := f.Info(); err == nil {
-					child := &FileTree{
-						Name: f.Name(),
-						Path: childPath,
-						Type: fileType,
-						Size: info.Size(),
+			// If the child is a directory, generate its file tree recursively
+			if f.IsDir() {
+				subTree := &FileTree{
+					Name:      fileName,
+					Path:      childPath,
+					Name2File: make(map[string]*FileTree),
+				}
+				tree.Name2File[fileName] = subTree
+				createSubTree(subTree, childPath)
+			} else {
+				// If the child is a file, add it to the file tree
+				if fileType := classifier(fileName); fileType != "" {
+					if info, err := f.Info(); err == nil {
+						tree.Name2File[fileName] = &FileTree{
+							Name: fileName,
+							Path: childPath,
+							Type: fileType,
+							Size: info.Size(),
+						}
 					}
-					tree.Children = append(tree.Children, child)
 				}
 			}
 		}
 	}
-
-	return tree, nil
+	rootTree := &FileTree{
+		Name:      filepath.Base(root),
+		Path:      root,
+		Name2File: make(map[string]*FileTree),
+	}
+	createSubTree(rootTree, root)
+	return rootTree
 }
 
 const configFileName = "meditree.json"
@@ -108,7 +117,7 @@ func main() {
 		}
 		return ""
 	}
-	tree, err := CreateFileTree(config["root"].(string), fileClassifier)
+	tree := CreateFileTree(config["root"].(string), fileClassifier)
 	jsonData, err := json.MarshalIndent(tree.ToJson(), "", " ")
 	if err != nil {
 		fmt.Println(err)
@@ -121,6 +130,12 @@ func main() {
 		_, _ = w.Write(jsonData)
 	})
 
-	fmt.Println("Server is running on http://localhost")
-	_ = http.ListenAndServe(":80", nil)
+	port := int(config["port"].(float64))
+	err = http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+		return
+	}
+	fmt.Printf("Server is running on http://localhost:%v\n", port)
 }
