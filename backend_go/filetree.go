7e0a54fb009b80402bd2e13c5654cdc68293581e
Liplum <Li_plum@outlook.com>
2023-03-28 17:11:52 +0800

    port go backend to new file list payload

--- backend_go/filetree.go
+++ backend_go/filetree.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"os"
 	"path/filepath"
 )
@@ -12,14 +10,18 @@ type FileTree struct {
 	Name     string
 	Path     string
 	Type     FileType
+	Size     int
 	Children []*FileTree
 }
 
-func (tree *FileTree) ToJson() any {
+func (tree *FileTree) ToJson() map[string]any {
 	res := make(map[string]any)
 	for _, file := range tree.Children {
 		if file.Children == nil {
-			res[file.Name] = "text/plain"
+			res[file.Name] = map[string]any{
+				"size": file.Size,
+				"type": file.Type,
+			}
 		} else {
 			res[file.Name] = file.ToJson()
 		}
@@ -52,38 +54,16 @@ func CreateFileTree(path string, classifier func(path string) FileType) (*FileTr
 			tree.Children = append(tree.Children, child)
 		} else {
 			// If the child is a file, add it to the file tree
-			child := &FileTree{
-				Name: f.Name(),
-				Path: childPath,
+			if fileType := classifier(childPath); fileType != "" {
+				child := &FileTree{
+					Name: f.Name(),
+					Path: childPath,
+					Type: fileType,
+				}
+				tree.Children = append(tree.Children, child)
 			}
-			tree.Children = append(tree.Children, child)
 		}
 	}
 
 	return tree, nil
-}
-
-func main() {
-	// Set the root directory path
-	root := "."
-
-	// Generate the file tree
-	tree, err := CreateFileTree(root, func(path string) FileType {
-		return ""
-	})
-
-	if err != nil {
-		fmt.Println("Error generating file tree:", err)
-		os.Exit(1)
-	}
-	jobj := tree.ToJson()
-	// Serialize the file tree to JSON
-	jsonData, err := json.MarshalIndent(jobj, "", "  ")
-	if err != nil {
-		fmt.Println("Error serializing file tree to JSON:", err)
-		os.Exit(1)
-	}
-
-	// Print the file tree
-	fmt.Printf("%s\n", jsonData)
 }
