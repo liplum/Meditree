bd05074869898a70af01c13a950215ed32dcacbd
Liplum <Li_plum@outlook.com>
2023-04-01 08:21:42 +0800

    extract crypt.ts

--- backend-js/src/meditree.ts
+++ backend-js/src/meditree.ts
@@ -2,7 +2,6 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import WebSocket, { WebSocketServer } from "ws"
 import { createLogger, type Logger } from "./logger.js"
-import nacl from "tweetnacl"
 import { v4 as uuidv4 } from "uuid"
 import { Net, MessageType } from "./net.js"
 import { LocalFile, type FileTreeLike, type FileTreeJson, type File, type RemoteFile } from "./file.js"
@@ -11,6 +10,7 @@ import { type Readable } from "stream"
 import fs from "fs"
 import { ForwardType, type ForwardConfig, type AsCentralConfig, type AsNodeConfig, type CentralConfig } from "./config.js"
 import type expressWs from "express-ws"
+import { encrypt, decrypt, generateNonce } from "./crypt.js"
 
 class SubNode implements FileTreeLike {
   readonly name: string
@@ -271,7 +271,7 @@ async function authenticateNodeAsCentral(
   log: Logger,
   config: AsCentralConfig,
 ): Promise<NodeMeta | undefined> {
-  const nonce = nacl.randomBytes(24)
+  const nonce = generateNonce()
   const challenge = uuidv4()
   const { publicKey }: { publicKey: string } = await net.getMessage("auth-public-key")
   if (!config.node.includes(publicKey)) throw new Error(`${publicKey} unregistered.`)
@@ -363,40 +363,6 @@ async function authenticateAsNode(
   return {
     name,
   }
-}
-
-function castUint8Array(raw: string | Uint8Array, encoding?: BufferEncoding): Uint8Array {
-  return raw instanceof Uint8Array ? raw : Buffer.from(raw, encoding)
-}
-
-function encrypt(
-  plain: Uint8Array | string,
-  nonce: Uint8Array | string,
-  theirPublicKey: Uint8Array | string,
-  myPrivateKey: Uint8Array | string
-): string {
-  const encrypted = nacl.box(
-    castUint8Array(plain),
-    castUint8Array(nonce, "base64"),
-    castUint8Array(theirPublicKey, "base64"),
-    castUint8Array(myPrivateKey, "base64"),
-  )
-  return Buffer.from(encrypted).toString("base64")
-}
-
-function decrypt(
-  encrypted: Uint8Array | string,
-  nonce: Uint8Array | string,
-  theirPublicKey: Uint8Array | string,
-  myPrivateKey: Uint8Array | string
-): string | null {
-  const decrypted = nacl.box.open(
-    castUint8Array(encrypted, "base64"),
-    castUint8Array(nonce, "base64"),
-    castUint8Array(theirPublicKey, "base64"),
-    castUint8Array(myPrivateKey, "base64"),
-  )
-  return decrypted === null ? null : Buffer.from(decrypted).toString()
 }
 
 function convertUrlToWs(mayBeUrl: string): string {
