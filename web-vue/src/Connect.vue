0a761301532bb37535d36963131ad0b3d6d4ff73
Liplum <Li_plum@outlook.com>
2023-04-05 19:18:53 +0800

    [web-vue] error dialog

--- web-vue/src/Connect.vue
+++ web-vue/src/Connect.vue
@@ -7,6 +7,7 @@ const serverRules = [(value) => checkServer(value)];
 const passcode = ref("");
 const showPasscode = ref(false);
 const isConnecting = ref(false);
+const showErrorDialog = ref(false);
 async function connect(event) {
   const result = await event;
   if (!result.valid) {
@@ -22,8 +23,10 @@ async function connect(event) {
       emit("list", payload);
     } else {
       const payload = await res.json();
-      return;
+      showErrorDialog.value = true;
     }
+  } catch (error) {
+    showErrorDialog.value = true;
   } finally {
     isConnecting.value = false;
   }
@@ -36,48 +39,58 @@ async function checkServer(server) {
 <template>
   <div class="dialog">
     <v-sheet rounded width="450" class="mx-auto sheet">
-      <h2 class="title">Connect to server</h2>
+      <v-card-title>
+        <h2 style="text-align: center">Connect to server</h2>
+      </v-card-title>
       <v-form
         validate-on="submit"
         @submit.prevent="connect"
         :disabled="isConnecting"
       >
-        <v-text-field
-          v-model="server"
-          :rules="serverRules"
-          label="Server"
-        ></v-text-field>
+        <v-card-text>
+          <v-text-field
+            v-model="server"
+            :rules="serverRules"
+            label="Server"
+          ></v-text-field>
 
-        <v-text-field
-          v-model="passcode"
-          :append-icon="showPasscode ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="showPasscode ? 'text' : 'password'"
-          placeholder="Optional"
-          label="Passcode"
-          @click:append="showPasscode = !showPasscode"
-        ></v-text-field>
-
-        <v-btn :disabled="isConnecting" rounded type="submit" block>
-          Connect
-        </v-btn>
+          <v-text-field
+            v-model="passcode"
+            :append-icon="showPasscode ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="showPasscode ? 'text' : 'password'"
+            placeholder="Optional"
+            label="Passcode"
+            @click:append="showPasscode = !showPasscode"
+          ></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" :disabled="isConnecting" rounded type="submit">
+            Connect
+          </v-btn>
+        </v-card-actions>
       </v-form>
     </v-sheet>
   </div>
+
+  <v-dialog v-model="showErrorDialog" width="auto">
+    <v-card>
+      <v-card-text>
+        Failed to load.
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary" @click="showErrorDialog = false">Close</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <style scoped>
-.title {
-  margin: 1rem;
-  text-align: center;
-}
 .dialog {
   display: flex;
   place-items: center;
   min-height: 100vh;
 }
-
 .sheet {
   border-radius: 16px;
-  padding: 1rem;
 }
 </style>
