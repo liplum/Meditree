b7aa458560063c61c8ac9c57a9cdf85af3500a1b
Liplum <Li_plum@outlook.com>
2023-04-09 17:37:01 +0800

    parse file and dir from payload

--- web-vue/src/FileTree.ts
+++ web-vue/src/FileTree.ts
@@ -1,14 +1,54 @@
-export interface FileInfo {
+export class FileInfo {
+  name: string
+  type: string
+  hidden: boolean = false
+  path: string;
+  size?: string;
+}
+
+export class DirectoryInfo {
+  name: string
+  hidden: boolean = false
+  files: (FileInfo | DirectoryInfo)[] = []
+}
+
+interface File {
   "*type": string;
   "*hide"?: boolean;
   path: string;
-  size: string;
+  size?: string;
 }
-export interface DirectoryInfo {
+
+interface Directory {
   "*hide"?: boolean;
-  [name: string]: FileInfo | DirectoryInfo | any;
+  [name: string]: File | Directory | any;
 }
-export interface FileTreeInfo {
-  name: string
-  files: DirectoryInfo
+
+function parseFile(name: string, file: File): FileInfo {
+  const fileInfo = new FileInfo();
+  fileInfo.name = name;
+  fileInfo.type = file["*type"];
+  fileInfo.hidden = file["*hide"] || false;
+  fileInfo.path = file.path;
+  fileInfo.size = file.size;
+  return fileInfo;
+}
+
+function parseDirectory(name: string, directory: Directory): DirectoryInfo {
+  const directoryInfo = new DirectoryInfo();
+  directoryInfo.name = name;
+  directoryInfo.hidden = directory["*hide"] || false;
+  directoryInfo.files = Object.keys(directory)
+    .filter((key) => key !== "*hide")
+    .map((key) => {
+      const entry = directory[key];
+      if (entry.hasOwnProperty("*type")) {
+        // Parse as file
+        return parseFile(key, entry as File);
+      } else {
+        // Parse as directory
+        return parseDirectory(key, entry as Directory);
+      }
+    });
+  return directoryInfo;
 }
