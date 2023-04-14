50bab1605d6d17ebec61da4dd5df11b25114399c
Liplum <Li_plum@outlook.com>
2023-04-15 02:07:57 +0800

    [web-vue] better display board

--- web-vue/src/Display/DisplayBoard.vue
+++ web-vue/src/Display/DisplayBoard.vue
@@ -21,10 +21,19 @@ const renderer = computed(() => {
     </v-app-bar>
     <v-main>
       <template v-if="props.file">
-        <v-responsive aspect-ratio="1" class="border pa-4">
-          <component :is="renderer" :file="props.file"></component>
-        </v-responsive>
+        <div class="image-container">
+          <v-responsive aspect-ratio="1" class="border pa-4">
+            <component :is="renderer" :file="props.file"></component>
+          </v-responsive>
+        </div>
       </template>
     </v-main>
   </v-layout>
 </template>
+
+<style>
+.image-container {
+  height: calc(100vh - 128px); /* adjust the height as needed */
+  overflow-y: auto;
+}
+</style>
