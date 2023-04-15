934eddbe0a0c0946778efdf7bf3c986156bafed7
Liplum <Li_plum@outlook.com>
2023-04-15 14:39:39 +0800

    [backend-spring] file tree skeleton

--- backend-spring/src/main/kotlin/File.kt
+++ backend-spring/src/main/kotlin/File.kt
@@ -3,6 +3,31 @@ package net.liplum.meditree
 import java.nio.file.Files
 import java.nio.file.Paths
 
+interface IFile {
+    val name: String
+    val path: String
+    val size: Int
+}
+
+class LocalFile(
+    val parent: LocalFileTree,
+    override val name: String,
+    override val path: String,
+    override val size: Int,
+) : IFile {
+}
+
+class LocalFileTree(
+    val parent: LocalFileTree?,
+    val path: String,
+) {
+
+}
+
+fun buildLocalFileTree(){
+
+}
+
 fun findFsEntryInTree(root: String, fileName: String): String? {
     var dir = root
     var lastDir: String? = null
