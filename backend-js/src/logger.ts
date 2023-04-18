aba1cc8f5ffa6e001970d0331a853839ef9e6e05
Liplum <Li_plum@outlook.com>
2023-04-19 03:28:44 +0800

    [backend-js] seperate `Timer` from `logger.ts`

--- backend-js/src/logger.ts
+++ backend-js/src/logger.ts
@@ -108,36 +108,3 @@ class LoggerImpl implements Logger {
 function tint(text: string, color?: ChalkInstance): string {
   return color ? color(text) : text
 }
-
-export class Timer {
-  private startTime?: number
-  private name2StartTime?: Record<string, number>
-  start(name?: string): void {
-    if (name) {
-      this.name2StartTime = { [name]: Date.now() }
-    } else {
-      this.startTime = Date.now()
-    }
-  }
-
-  stop(name?: string, log?: (time: string) => void): number {
-    let startTime: number | undefined
-    if (name) {
-      startTime = this.name2StartTime?.[name]
-    } else {
-      startTime = this.startTime
-    }
-    let time: number
-    if (startTime) {
-      time = Date.now() - startTime
-    } else {
-      time = 0
-    }
-    if (name) {
-      log?.(`${name}: ${time}ms`)
-    } else {
-      log?.(`${time}ms`)
-    }
-    return time
-  }
-}
