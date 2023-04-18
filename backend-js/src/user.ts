f825804c7d118ac2c238ad81b94073108b71dcb1
Liplum <Li_plum@outlook.com>
2023-04-19 03:11:38 +0800

    [backend-js] a skeleton of user module

---
+++ backend-js/src/user.ts
@@ -0,0 +1,40 @@
+/**
+ * Represents a user with an account and password.
+ */
+export interface User {
+  account: string
+  password: string
+}
+
+/**
+ * Represents a service to be injected for storing and manipulating users.
+ */
+export interface UserStorageService {
+  /**
+   * Adds a new user to the storage.
+   * @param user The user to add.
+   * @returns `true` if the user was added successfully, `false` otherwise.
+   */
+  addUser(user: User): boolean
+
+  /**
+   * Retrieves a user from the storage based on the account name.
+   * @param account The account name of the user to retrieve.
+   * @returns The retrieved user object or `undefined` if the user was not found.
+   */
+  getUser(account: string): User | undefined
+
+  /**
+   * Updates an existing user in the storage.
+   * @param user The updated user object.
+   * @returns `true` if the user was updated successfully, `false` otherwise.
+   */
+  updateUser(user: User): boolean
+
+  /**
+   * Deletes a user from the storage based on the account name.
+   * @param account The account name of the user to delete.
+   * @returns `true` if the user was deleted successfully, `false` otherwise.
+   */
+  deleteUser(account: string): boolean
+}
