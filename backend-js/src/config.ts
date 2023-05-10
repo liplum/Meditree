17fdfb356f363b5302f4ac0759fa1e8956498ce6
Liplum <Li_plum@outlook.com>
2023-05-10 23:56:12 +0800

    [backend-js] extract file finding module.

--- backend-js/src/config.ts
+++ backend-js/src/config.ts
@@ -3,6 +3,7 @@ import path from "path"
 import nacl from "tweetnacl"
 import { v4 as uuidv4 } from "uuid"
 import { HLSMediaType } from "./plugin/hls.js"
+import { findFSOInTreeByName, listAncestors } from "./file-finding.js"
 
 export interface AsParentConfig {
   name: string
@@ -150,45 +151,4 @@ export function findConfig({ rootDir, filename }: { rootDir: string, filename: s
     fs.writeFileSync(configFile, JSON.stringify(config, null, 2))
     throw new Error(`Configuration not found. ${configFile} is created.`)
   }
-}
-
-function findFSOInTreeByName(root: string, fileName: string): string | null {
-  return findFSOInTree(root, (dir) => path.join(dir, fileName))
-}
-
-function findFSOInTree(root: string, attemp: (dir: string) => string | undefined | null): string | null {
-  let dir = root
-  let lastDir: string | null = null
-  while (dir !== lastDir) {
-    const configFile = attemp(dir)
-    if (configFile && fs.existsSync(configFile)) {
-      return configFile
-    } else {
-      lastDir = dir
-      dir = path.dirname(dir)
-    }
-  }
-  return null
-}
-
-/**
- * List the existing ancestor directories based on {@link root} given,
- * starting with the nearest ancestor,
- * and ending with the root directory of file system.
-
- * @param root the root directory path to search
- * @returns a list of all existing ancestors.
- */
-function listAncestors(root: string): string[] {
-  const ancestors: string[] = []
-  let dir = root
-  let lastDir: string | null = null
-  while (dir !== lastDir) {
-    if (fs.existsSync(dir)) {
-      ancestors.push(dir)
-    }
-    lastDir = dir
-    dir = path.dirname(dir)
-  }
-  return ancestors
 }
