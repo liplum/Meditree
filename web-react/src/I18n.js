a7fb61587f729f9a44cbd71bafc80a15e6df0e07
Liplum <Li_plum@outlook.com>
2023-07-27 02:43:19 +0800

    [web-react] more l10n by chatGPT.

--- web-react/src/I18n.js
+++ web-react/src/I18n.js
@@ -2,25 +2,25 @@ import LocalizedStrings from "react-localization"
 
 export const i18n = new LocalizedStrings({
   en: {
-    noFile: "No File",
+    noFile: "No Files",
     search: {
-      starFilter: "Only Show Starred",
-      placeholder: "Search files or folders"
+      starFilter: "Show Favorites Only",
+      placeholder: "Search Files or Folders"
     },
     playground: {
-      starBtn: "Add To Star",
-      unstarBtn: "Remove From Star",
-      unsupportedFileType: "The file has an unsupported type."
+      starBtn: "Add to Favorites",
+      unstarBtn: "Remove from Favorites",
+      unsupportedFileType: "This file type is not supported."
     },
     loading: {
       text: "Loading...",
       failed: "Failed to load content.",
       error: {
-        incorrectPassword: "Incorrect Passcode",
+        incorrectPassword: "Incorrect Password",
       }
     },
     login: {
-      title: "Log in Meditree Server",
+      title: "Log into Meditree Server",
       account: "Account",
       password: "Password",
       accountPlaceholder: "Optional",
@@ -29,30 +29,111 @@ export const i18n = new LocalizedStrings({
     },
   },
   zh: {
-    noFile: "无文件",
+    noFile: "没有文件",
     search: {
-      starFilter: "仅显示已收藏",
+      starFilter: "仅显示收藏",
       placeholder: "搜索文件或文件夹"
     },
     playground: {
-      starBtn: "收藏",
+      starBtn: "添加收藏",
       unstarBtn: "取消收藏",
       unsupportedFileType: "文件类型不支持。"
     },
     loading: {
       text: "加载中……",
-      failed: "无法加载目录",
+      failed: "无法加载内容。",
       error: {
         incorrectPassword: "密码不正确",
       }
     },
     login: {
-      title: "登入 Meditree 服务器",
+      title: "登录 Meditree 服务器",
       account: "账号",
       password: "密码",
       accountPlaceholder: "可选",
       passwordPlaceholder: "可选",
-      loginBtn: "登入",
+      loginBtn: "登录",
     },
-  }
+  },
+  fr: {
+    noFile: "Aucun fichier",
+    search: {
+      starFilter: "Afficher uniquement les favoris",
+      placeholder: "Rechercher des fichiers ou des dossiers"
+    },
+    playground: {
+      starBtn: "Ajouter aux favoris",
+      unstarBtn: "Retirer des favoris",
+      unsupportedFileType: "Ce type de fichier n'est pas pris en charge."
+    },
+    loading: {
+      text: "Chargement...",
+      failed: "Impossible de charger le contenu.",
+      error: {
+        incorrectPassword: "Mot de passe incorrect",
+      }
+    },
+    login: {
+      title: "Connexion au serveur Meditree",
+      account: "Compte",
+      password: "Mot de passe",
+      accountPlaceholder: "Facultatif",
+      passwordPlaceholder: "Facultatif",
+      loginBtn: "Connexion",
+    },
+  },
+  ru: {
+    noFile: "Нет файлов",
+    search: {
+      starFilter: "Показать только избранное",
+      placeholder: "Поиск файлов или папок"
+    },
+    playground: {
+      starBtn: "Добавить в избранное",
+      unstarBtn: "Убрать из избранного",
+      unsupportedFileType: "Этот тип файла не поддерживается."
+    },
+    loading: {
+      text: "Загрузка...",
+      failed: "Не удалось загрузить содержимое.",
+      error: {
+        incorrectPassword: "Неверный пароль",
+      }
+    },
+    login: {
+      title: "Вход на сервер Meditree",
+      account: "Аккаунт",
+      password: "Пароль",
+      accountPlaceholder: "Необязательно",
+      passwordPlaceholder: "Необязательно",
+      loginBtn: "Войти",
+    },
+  },
+  uk: {
+    noFile: "Файлів немає",
+    search: {
+      starFilter: "Показати лише обрані",
+      placeholder: "Пошук файлів або папок"
+    },
+    playground: {
+      starBtn: "Додати до обраного",
+      unstarBtn: "Видалити з обраного",
+      unsupportedFileType: "Цей тип файлу не підтримується."
+    },
+    loading: {
+      text: "Завантаження...",
+      failed: "Не вдалося завантажити вміст.",
+      error: {
+        incorrectPassword: "Невірний пароль",
+      }
+    },
+    login: {
+      title: "Увійти на сервер Meditree",
+      account: "Обліковий запис",
+      password: "Пароль",
+      accountPlaceholder: "Необов'язково",
+      passwordPlaceholder: "Необов'язково",
+      loginBtn: "Увійти",
+    },
+  },
 })
