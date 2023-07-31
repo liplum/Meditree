36ff12aa58c9416045242b488951c159a77ecd95
Liplum <Li_plum@outlook.com>
2023-07-31 16:43:18 +0800

    [web-vue] imporved l10n

--- web-vue/src/i18n.js
+++ web-vue/src/i18n.js
@@ -1,23 +1,137 @@
 export const l10nChart = {
   en: {
-    appName: "Meditree",
+    noFile: "No Files",
+    search: {
+      starFilter: "Show Favorites Only",
+      placeholder: "Search Files or Folders"
+    },
+    display: {
+      starBtn: "Add to Favorites",
+      unstarBtn: "Remove from Favorites",
+      unsupportedFileType: "This file type is not supported."
+    },
+    loading: {
+      text: "Loading...",
+      failed: "Failed to load content.",
+      error: {
+        incorrectPassword: "Incorrect Password",
+      }
+    },
     login: {
-      title: "Login Meditree Server",
+      title: "Log into Meditree Server",
       account: "Account",
       password: "Password",
-      login: "Login",
-      error:{
-        failed: "Failed to Login."
-      }
-    }
+      accountPlaceholder: "Optional",
+      passwordPlaceholder: "Optional",
+      loginBtn: "Login",
+    },
   },
   zh: {
-    appName: "Meditree",
+    noFile: "没有文件",
+    search: {
+      starFilter: "仅显示收藏",
+      placeholder: "搜索文件或文件夹"
+    },
+    display: {
+      starBtn: "添加收藏",
+      unstarBtn: "取消收藏",
+      unsupportedFileType: "文件类型不支持。"
+    },
+    loading: {
+      text: "加载中……",
+      failed: "无法加载内容。",
+      error: {
+        incorrectPassword: "密码不正确",
+      }
+    },
     login: {
       title: "登录 Meditree 服务器",
       account: "账号",
       password: "密码",
-      login: "登录",
-    }
-  }
+      accountPlaceholder: "可选",
+      passwordPlaceholder: "可选",
+      loginBtn: "登录",
+    },
+  },
+  fr: {
+    noFile: "Aucun fichier",
+    search: {
+      starFilter: "Afficher uniquement les favoris",
+      placeholder: "Rechercher des fichiers ou des dossiers"
+    },
+    display: {
+      starBtn: "Ajouter aux favoris",
+      unstarBtn: "Retirer des favoris",
+      unsupportedFileType: "Ce type de fichier n'est pas pris en charge."
+    },
+    loading: {
+      text: "Chargement...",
+      failed: "Impossible de charger le contenu.",
+      error: {
+        incorrectPassword: "Mot de passe incorrect",
+      }
+    },
+    login: {
+      title: "Connexion au serveur Meditree",
+      account: "Compte",
+      password: "Mot de passe",
+      accountPlaceholder: "Facultatif",
+      passwordPlaceholder: "Facultatif",
+      loginBtn: "Connexion",
+    },
+  },
+  ru: {
+    noFile: "Нет файлов",
+    search: {
+      starFilter: "Показать только избранное",
+      placeholder: "Поиск файлов или папок"
+    },
+    display: {
+      starBtn: "Добавить в избранное",
+      unstarBtn: "Убрать из избранного",
+      unsupportedFileType: "Этот тип файла не поддерживается."
+    },
+    loading: {
+      text: "Загрузка...",
+      failed: "Не удалось загрузить содержимое.",
+      error: {
+        incorrectPassword: "Неверный пароль",
+      }
+    },
+    login: {
+      title: "Вход на сервер Meditree",
+      account: "Аккаунт",
+      password: "Пароль",
+      accountPlaceholder: "Необязательно",
+      passwordPlaceholder: "Необязательно",
+      loginBtn: "Войти",
+    },
+  },
+  uk: {
+    noFile: "Файлів немає",
+    search: {
+      starFilter: "Показати лише обрані",
+      placeholder: "Пошук файлів або папок"
+    },
+    display: {
+      starBtn: "Додати до обраного",
+      unstarBtn: "Видалити з обраного",
+      unsupportedFileType: "Цей тип файлу не підтримується."
+    },
+    loading: {
+      text: "Завантаження...",
+      failed: "Не вдалося завантажити вміст.",
+      error: {
+        incorrectPassword: "Невірний пароль",
+      }
+    },
+    login: {
+      title: "Увійти на сервер Meditree",
+      account: "Обліковий запис",
+      password: "Пароль",
+      accountPlaceholder: "Необов'язково",
+      passwordPlaceholder: "Необов'язково",
+      loginBtn: "Увійти",
+    },
+  },
 }
