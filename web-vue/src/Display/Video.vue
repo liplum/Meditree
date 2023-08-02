64e66f7bbf0f1e941daa88224a51a042d171e5ad
Liplum <Li_plum@outlook.com>
2023-08-02 15:30:10 +0800

    [web-vue] switching videos

--- web-vue/src/Display/Video.vue
+++ web-vue/src/Display/Video.vue
@@ -1,12 +1,17 @@
 <script lang="ts" setup>
 import { FileInfo } from '../FileTree'
 import VideoPlayer from "../Component/VideoPlayer.vue"
+import { watch, reactive } from 'vue';
+
 const props = defineProps<{
   file: FileInfo
 }>()
+
 const isMobileSafari = /iP(ad|hone|od).+Version\/[\d.]+.*Safari/i.test(navigator.userAgent)
 const overrideNative = !isMobileSafari
-const videoOptions = {
+
+// Create a ref to store the options
+const options = reactive({
   controls: true,
   responsive: true,
   liveui: true,
@@ -23,11 +28,22 @@ const videoOptions = {
     src: props.file.url,
     type: props.file.type,
   }],
-}
+});
+
+// Watch for changes in the 'file' prop and update the 'options'
+watch(() => props.file, (newFile) => {
+  options.sources = [{
+    src: newFile.url,
+    type: newFile.type,
+  }]
+});
+
 </script>
 
 <template>
   <v-responsive class="border pa-4 container">
-    <video-player :options="videoOptions" />
+    <div :key="props.file.url">
+      <video-player :options="options" />
+    </div>
   </v-responsive>
 </template>
