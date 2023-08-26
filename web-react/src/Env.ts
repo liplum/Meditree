1f195aa3b8ed02931e700b4fad24b2a1f0b7e81b
Liplum <Li_plum@outlook.com>
2023-08-27 04:35:23 +0800

    [web-react] typing env

--- web-react/src/Env.ts
+++ web-react/src/Env.ts
@@ -1,12 +1,12 @@
-export function updatePageTitle(title:string) {
+export function updatePageTitle(title: string) {
   document.title = `${title} - Meditree`
 }
 
 export const storage = {
-  get lastFilePathFromUrl() {
+  get lastFilePathFromUrl(): string | null {
     return window.localStorage.getItem("last-file-path-from-url")
   },
-  set lastFilePathFromUrl(value) {
+  set lastFilePathFromUrl(value: string | null) {
     if (value) window.localStorage.setItem("last-file-path-from-url", value)
     else window.localStorage.removeItem("last-file-path-from-url")
   },
