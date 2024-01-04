9f9fc0bb1d465e7712ecba680d590760609e19a1
Liplum <Li_plum@outlook.com>
2024-01-05 01:48:13 +0800

    [shorts] play random video

--- shorts/src/App.tsx
+++ shorts/src/App.tsx
@@ -5,52 +5,17 @@ import TopNavbar from './components/TopNavbar';
 import './App.css';
 
 // This array holds information about different videos
-const videoUrls = [
-  {
-    url: '/videos/video1.mp4',
-    profilePic: 'https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-giso/9d429ac49d6d18de6ebd2a3fb1f39269~c5_100x100.jpeg?x-expires=1688479200&x-signature=pjH5pwSS8Sg1dJqbB1GdCLXH6ew%3D',
-    username: 'csjackie',
-    description: 'Lol nvm #compsci #chatgpt #ai #openai #techtok',
-    song: 'Original sound - Famed Flames',
-    likes: 430,
-    comments: 13,
-    saves: 23,
-    shares: 1,
-  },
-  {
-    url: '/videos/video2.mp4',
-    profilePic: 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/eace3ee69abac57c39178451800db9d5~c5_100x100.jpeg?x-expires=1688479200&x-signature=wAkVmwL7lej15%2B16ypSWQOqTP8s%3D',
-    username: 'dailydotdev',
-    description: 'Every developer brain @francesco.ciulla #developerjokes #programming #programminghumor #programmingmemes',
-    song: 'tarawarolin wants you to know this isnt my sound - Chaplain J Rob',
-    likes: '13.4K',
-    comments: 3121,
-    saves: 254,
-    shares: 420,
-  },
-  {
-    url: '/videos/video3.mp4',
-    profilePic: 'https://p77-sign-va.tiktokcdn.com/tos-maliva-avt-0068/4e6698b235eadcd5d989a665704daf68~c5_100x100.jpeg?x-expires=1688479200&x-signature=wkwHDKfNuIDqIVHNm29%2FRf40R3w%3D',
-    username: 'wojciechtrefon',
-    description: '#programming #softwareengineer #vscode #programmerhumor #programmingmemes',
-    song: 'help so many people are using my sound - Ezra',
-    likes: 5438,
-    comments: 238,
-    saves: 12,
-    shares: 117,
-  },
-  {
-    url: '/videos/video4.mp4',
-    profilePic: 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/4bda52cf3ad31c728153859262c329db~c5_100x100.jpeg?x-expires=1688486400&x-signature=ssUbbCpZFJj6uj33D%2BgtcqxMvgQ%3D',
-    username: 'faruktutkus',
-    description: 'Wait for the end | Im RTX 4090 TI | #softwareengineer #softwareengineer #coding #codinglife #codingmemes ',
-    song: 'orijinal ses - Computer Science',
-    likes: 9689,
-    comments: 230,
-    saves: 1037,
-    shares: 967,
-  },
-];
+const videoUrls = Array(20).fill({
+  url: '/api/random-file?type=video',
+  profilePic: 'https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-giso/9d429ac49d6d18de6ebd2a3fb1f39269~c5_100x100.jpeg?x-expires=1688479200&x-signature=pjH5pwSS8Sg1dJqbB1GdCLXH6ew%3D',
+  username: 'csjackie',
+  description: 'Lol nvm #compsci #chatgpt #ai #openai #techtok',
+  song: 'Original sound - Famed Flames',
+  likes: 430,
+  comments: 13,
+  saves: 23,
+  shares: 1,
+});
 
 function App() {
   const [videos, setVideos] = useState([]);
@@ -106,6 +71,8 @@ function App() {
         {videos.map((video, index) => (
           <VideoCard
             key={index}
+            autoplay
+            loop
             username={video.username}
             description={video.description}
             song={video.song}
@@ -116,14 +83,13 @@ function App() {
             url={video.url}
             profilePic={video.profilePic}
             setVideoRef={handleVideoRef(index)}
-            autoplay={index === 0}
           />
         ))}
         <BottomNavbar className="bottom-navbar" />
       </div>
     </div>
   );
-  
+
 }
 
 export default App;
--- shorts/vite.config.ts
+++ shorts/vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
-    port: 23456,
+    port: 8080,
   },
 })
