ed8a49df17ae25df1cb7eebfae89a4d60422ea34
Liplum <Li_plum@outlook.com>
2024-03-01 03:57:32 +0800

    thanks to https://github.com/mjl0602/flutter_tiktok

---
+++ app-flutter/lib/pages/homePage.dart
@@ -0,0 +1,253 @@
+import 'package:meditree/mock/video.dart';
+import 'package:meditree/pages/cameraPage.dart';
+import 'package:meditree/pages/followPage.dart';
+import 'package:meditree/pages/searchPage.dart';
+import 'package:meditree/pages/userPage.dart';
+import 'package:meditree/style/physics.dart';
+import 'package:meditree/views/tikTokCommentBottomSheet.dart';
+import 'package:meditree/views/tikTokHeader.dart';
+import 'package:meditree/views/tikTokScaffold.dart';
+import 'package:meditree/views/tikTokVideo.dart';
+import 'package:meditree/views/tikTokVideoButtonColumn.dart';
+import 'package:meditree/controller/tikTokVideoListController.dart';
+import 'package:meditree/views/tiktokTabBar.dart';
+import 'package:flutter/material.dart';
+import 'package:safemap/safemap.dart';
+import 'package:video_player/video_player.dart';
+
+import 'msgPage.dart';
+
+/// 单独修改了bottomSheet组件的高度
+import 'package:meditree/other/bottomSheet.dart' as CustomBottomSheet;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with WidgetsBindingObserver {
+  TikTokPageTag tabBarType = TikTokPageTag.home;
+
+  TikTokScaffoldController tkController = TikTokScaffoldController();
+
+  PageController _pageController = PageController();
+
+  TikTokVideoListController _videoListController = TikTokVideoListController();
+
+  /// 记录点赞
+  Map<int, bool> favoriteMap = {};
+
+  List<UserVideo> videoDataList = [];
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) async {
+    if (state != AppLifecycleState.resumed) {
+      _videoListController.currentPlayer.pause();
+    }
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    _videoListController.currentPlayer.pause();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    videoDataList = UserVideo.fetchVideo();
+    WidgetsBinding.instance.addObserver(this);
+    _videoListController.init(
+      pageController: _pageController,
+      initialList: videoDataList
+          .map(
+            (e) => VPVideoController(
+              videoInfo: e,
+              builder: () => VideoPlayerController.network(e.url),
+            ),
+          )
+          .toList(),
+      videoProvider: (int index, List<VPVideoController> list) async {
+        return videoDataList
+            .map(
+              (e) => VPVideoController(
+                videoInfo: e,
+                builder: () => VideoPlayerController.network(e.url),
+              ),
+            )
+            .toList();
+      },
+    );
+    _videoListController.addListener(() {
+      setState(() {});
+    });
+    tkController.addListener(
+      () {
+        if (tkController.value == TikTokPagePositon.middle) {
+          _videoListController.currentPlayer.play();
+        } else {
+          _videoListController.currentPlayer.pause();
+        }
+      },
+    );
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget? currentPage;
+
+    switch (tabBarType) {
+      case TikTokPageTag.home:
+        break;
+      case TikTokPageTag.follow:
+        currentPage = FollowPage();
+        break;
+      case TikTokPageTag.msg:
+        currentPage = MsgPage();
+        break;
+      case TikTokPageTag.me:
+        currentPage = UserPage(isSelfPage: true);
+        break;
+    }
+    double a = MediaQuery.of(context).size.aspectRatio;
+    bool hasBottomPadding = a < 0.55;
+
+    bool hasBackground = hasBottomPadding;
+    hasBackground = tabBarType != TikTokPageTag.home;
+    if (hasBottomPadding) {
+      hasBackground = true;
+    }
+    Widget tikTokTabBar = TikTokTabBar(
+      hasBackground: hasBackground,
+      current: tabBarType,
+      onTabSwitch: (type) async {
+        setState(() {
+          tabBarType = type;
+          if (type == TikTokPageTag.home) {
+            _videoListController.currentPlayer.play();
+          } else {
+            _videoListController.currentPlayer.pause();
+          }
+        });
+      },
+      onAddButton: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            fullscreenDialog: true,
+            builder: (context) => CameraPage(),
+          ),
+        );
+      },
+    );
+
+    var userPage = UserPage(
+      isSelfPage: false,
+      canPop: true,
+      onPop: () {
+        tkController.animateToMiddle();
+      },
+    );
+    var searchPage = SearchPage(
+      onPop: tkController.animateToMiddle,
+    );
+
+    var header = tabBarType == TikTokPageTag.home
+        ? TikTokHeader(
+            onSearch: () {
+              tkController.animateToLeft();
+            },
+          )
+        : Container();
+
+    // 组合
+    return TikTokScaffold(
+      controller: tkController,
+      hasBottomPadding: hasBackground,
+      tabBar: tikTokTabBar,
+      header: header,
+      leftPage: searchPage,
+      rightPage: userPage,
+      enableGesture: tabBarType == TikTokPageTag.home,
+      // onPullDownRefresh: _fetchData,
+      page: Stack(
+        // index: currentPage == null ? 0 : 1,
+        children: <Widget>[
+          PageView.builder(
+            key: Key('home'),
+            physics: QuickerScrollPhysics(),
+            controller: _pageController,
+            scrollDirection: Axis.vertical,
+            itemCount: _videoListController.videoCount,
+            itemBuilder: (context, i) {
+              // 拼一个视频组件出来
+              bool isF = SafeMap(favoriteMap)[i].boolean;
+              var player = _videoListController.playerOfIndex(i)!;
+              var data = player.videoInfo!;
+              // 右侧按钮列
+              Widget buttons = TikTokButtonColumn(
+                isFavorite: isF,
+                onAvatar: () {
+                  tkController.animateToPage(TikTokPagePositon.right);
+                },
+                onFavorite: () {
+                  setState(() {
+                    favoriteMap[i] = !isF;
+                  });
+                  // showAboutDialog(context: context);
+                },
+                onComment: () {
+                  CustomBottomSheet.showModalBottomSheet(
+                    backgroundColor: Colors.white.withOpacity(0),
+                    context: context,
+                    builder: (BuildContext context) =>
+                        TikTokCommentBottomSheet(),
+                  );
+                },
+                onShare: () {},
+              );
+              // video
+              Widget currentVideo = Center(
+                child: AspectRatio(
+                  aspectRatio: player.controller.value.aspectRatio,
+                  child: VideoPlayer(player.controller),
+                ),
+              );
+
+              currentVideo = TikTokVideoPage(
+                // 手势播放与自然播放都会产生暂停按钮状态变化，待处理
+                hidePauseIcon: !player.showPauseIcon.value,
+                aspectRatio: 9 / 16.0,
+                key: Key(data.url + '$i'),
+                tag: data.url,
+                bottomPadding: hasBottomPadding ? 16.0 : 16.0,
+                userInfoWidget: VideoUserInfo(
+                  desc: data.desc,
+                  bottomPadding: hasBottomPadding ? 16.0 : 50.0,
+                ),
+                onSingleTap: () async {
+                  if (player.controller.value.isPlaying) {
+                    await player.pause();
+                  } else {
+                    await player.play();
+                  }
+                  setState(() {});
+                },
+                onAddFavorite: () {
+                  setState(() {
+                    favoriteMap[i] = true;
+                  });
+                },
+                rightButtonColumn: buttons,
+                video: currentVideo,
+              );
+              return currentVideo;
+            },
+          ),
+          currentPage ?? Container(),
+        ],
+      ),
+    );
+  }
+}
