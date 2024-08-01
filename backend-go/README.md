437a0d3aec52edbc57e8d04a54be0382f3c0c880
Liplum <Li_plum@outlook.com>
2024-08-02 02:21:43 +0800

    depreacated go and springboot back ends

--- backend-go/README.md
+++
@@ -1,5 +0,0 @@
-# Golang Backend
-
-## Getting Started
-
-Run `go build main.go` to compile and start the server.
--- backend-spring/src/main/kotlin/MeditreeConfiguration.kt
+++
@@ -1,9 +0,0 @@
-package net.liplum.meditree
-import kotlinx.serialization.*
-import org.springframework.boot.context.properties.ConfigurationProperties
-
-@Serializable
-@ConfigurationProperties(prefix = "meditree")
-data class MeditreeConfiguration(
-    val root: String? = null,
-)
